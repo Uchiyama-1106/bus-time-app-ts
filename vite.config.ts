a0bcdc7954728ce215b2df1a8a161b367f5413c1
Uchiyama-1106 <timehurosiki11068@gmail.com>
2025-05-25 12:50:53 +0900

    fix: modify vite.config.ts to ensure proper deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/bus-time-app-ts/', // リポジトリ名に合わせる
   plugins: [
     tailwindcss(),
     react(),
   ],
 })
+
